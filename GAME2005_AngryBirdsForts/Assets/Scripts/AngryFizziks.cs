c3e4653569ff6d77849c6a8fbbbba48209ac7e09
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2024-12-04 22:15:41 -0500

    Added AngryFizziks script. Created the base for the script

---
+++ GAME2005_AngryBirdsForts/Assets/Scripts/AngryFizziks.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngryFizziks : MonoBehaviour
+{
+    public List<AngryFizziks> angryFizziksList = new List<AngryFizziks>();
+    public float dT = 0.02f;
+
+    public Vector3 gravityAcceleration = new Vector3(0, -10, 0);
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
