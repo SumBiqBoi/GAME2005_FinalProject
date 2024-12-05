6fcc688038bc60f629ca4a29f6fdd6b716ef926f
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2024-12-04 22:12:20 -0500

    Added AngryShape script. Created the base for the script

---
+++ GAME2005_AngryBirdsForts/Assets/Scripts/AngryShapes.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class AngryShapes : MonoBehaviour
+{
+    public float mass = 1;
+    public float gravityScale = 0;
+    public float grippyness = 0.5f;
+
+    public Vector3 velocity = Vector3.zero;
+    public Vector3 netForce = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
